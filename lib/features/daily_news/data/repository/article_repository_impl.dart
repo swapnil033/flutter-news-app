59bf619170cc647828ee0615c3f1c69bf6ed8aa5
swapnil033 <swapnildev33@gmail.com>
2024-03-10 22:05:55 +0530

    create repository classes and model

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/constants.dart';
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:news_app/features/daily_news/data/models/article.dart';
+import 'package:news_app/features/daily_news/domain/entities/article.dart';
+import 'package:news_app/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+  @override
+  Future<DataState<List<ArticleEntity>>> getNewsArticles() {
+    // TODO: implement getNewsArticles
+    throw UnimplementedError();
+  }
+}
