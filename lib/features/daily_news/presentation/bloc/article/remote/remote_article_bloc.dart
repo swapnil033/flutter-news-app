ded8be22b29d23e3d0c4da4fa3da55ea538e1abd
swapnil033 <swapnildev33@gmail.com>
2024-03-10 22:12:49 +0530

    create bloc

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/resources/data_state.dart';
+import 'package:news_app/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticlesEvent, RemoteArticleState> {
+  final GetArticleUseCase _articleUseCase;
+
+  RemoteArticlesBloc(this._articleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticles>(onGetArticles);
+  }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _articleUseCase();
+
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesDone(dataState.data!));
+    }
+
+    if (dataState is DataFailed) {
+      emit(RemoteArticlesError(dataState.error!));
+    }
+  }
+}
